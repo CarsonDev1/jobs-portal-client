246556399eabb5b6dd08117e627800770984ad1b
CarsonDev1 <buitritinht@gmail.com>
2025-09-30 09:24:54 +0700

    add host allow

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://222.255.119.33:5010'
+const API_BASE = import.meta.env.VITE_API_BASE || 'https://api.allorigins.win/raw?url=' + encodeURIComponent('http://222.255.119.33:5010')
 
 export const api = axios.create({
   baseURL: API_BASE,
